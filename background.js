554608d1773f1ed5973f90af38e3e27b9acbe9a6
Lionel Wong <lionelster@gmail.com>
2020-10-25 13:43:44 +0800

    Rename select-functions

--- background.js
+++ background.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const menu = {
-    select_parent: `&Parent`,
-    select_parent_descendants: `P&arent and Descendants`,
-    select_siblings_descendants: `&Siblings and Descendants`,
-    select_tab_descendants: `&Tab and Descendants`,
-    select_descendants: `&Descendants`,
-    select_site: `Sa&me Site Tabs`,
-    select_site_descendants: `Sam&e Site Tabs and Descendants`,
+    // Menu item id/Function name   // Title
+    selectParent:                   '&Parent',
+    selectParentAndDescendants:     'P&arent and Descendants',
+    selectSiblingsAndDescendants:   '&Siblings and Descendants',
+    selectTargetAndDescendants:     '&Tab and Descendants',
+    selectDescendants:              '&Descendants',
+    selectSite:                     'Sa&me Site Tabs',
+    selectSiteAndDescendants:       'Sam&e Site Tabs and Descendants',
 }
 
 for (const id in menu) {
@@ -19,42 +20,42 @@ for (const id in menu) {
     });
 }
 
-function select_parent(_, tab) {
+function selectParent(_, tab) {
     const parentTabId = tab.openerTabId;
     if (parentTabId) {
         browser.tabs.update(parentTabId, { active: true });
     }
 }
 
-async function select_parent_descendants(_, tab) {
+async function selectParentAndDescendants(_, tab) {
     const parentTabId = tab.openerTabId;
     if (parentTabId) {
         const tabPromises = [browser.tabs.get(parentTabId), getDescendants(parentTabId)];
         const familyTabs = (await Promise.all(tabPromises)).flat();
         select(familyTabs);
     } else {
-        select_tab_descendants(_, tab);
+        selectTargetAndDescendants(_, tab);
     }
 }
 
-async function select_siblings_descendants(_, tab) {
+async function selectSiblingsAndDescendants(_, tab) {
     const familyTabs = await getDescendants(tab.openerTabId);
     select(familyTabs);
 }
 
-async function select_tab_descendants(_, tab) {
+async function selectTargetAndDescendants(_, tab) {
     const familyTabs = [tab].concat(await getDescendants(tab.id));
     select(familyTabs);
 }
 
-async function select_descendants(_, tab) {
+async function selectDescendants(_, tab) {
     const descendantTabs = await getDescendants(tab.id);
     if (descendantTabs.length) {
         select(descendantTabs);
     }
 }
 
-async function select_site_descendants(_, tab) {
+async function selectSiteAndDescendants(_, tab) {
     const siteTabs = getSiteTabs(tab);
     let familyTabs = [].concat(siteTabs);
     for (const tab of siteTabs) {
@@ -63,7 +64,7 @@ async function select_site_descendants(_, tab) {
     select(familyTabs);
 }
 
-async function select_site(_, tab) {
+async function selectSite(_, tab) {
     select(await getSiteTabs(tab));
 }
 

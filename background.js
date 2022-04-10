2a62366c2bf0b59b361732770e516863802798fa
Lionel Wong <lionelster@gmail.com>
2022-04-10 17:42:13 +0800

    Disable commands via stored preferences

--- background.js
+++ background.js
@@ -2,10 +2,20 @@ import * as Getter from './get.js';
 import selectTabs from './select.js';
 import menuData from './menudata.js';
 
-buildMenu(menuData);
+init();
+
+async function init() {
+    const preferences = await browser.storage.sync.get();
+
+    buildMenu(menuData, new Set(preferences.disabledCommands));
+
+    browser.contextMenus.onClicked.addListener(
+        ({ menuItemId }, tab) => selectTabs(Getter[menuItemId], tab)
+    );
+}
 
 // menuGroupDict is an dict of group titles mapped to dicts of getter names mapped to getter titles
-function buildMenu(menuGroupDict) {
+function buildMenu(menuGroupDict, disabledItemSet) {
     const contexts = ['tab'];
     const parentId = 'selecttabs';
     const parentTitle = '&Select Tabs';
@@ -14,18 +24,22 @@ function buildMenu(menuGroupDict) {
     const addItem = (id, title) => browser.contextMenus.create({ contexts, parentId, id, title });
     const addSeparator = ()     => browser.contextMenus.create({ contexts, parentId, type: 'separator' });
 
+    const removeDisabled = disabledItemSet.size ?
+        (_, id, map) => {
+            if (disabledItemSet.has(id))
+                map.delete(id);
+        } : null;
+
     addRoot();
 
     const menuGroups = Object.values(menuGroupDict);
     for (let i = 0, n = menuGroups.length; i < n; i++) {
-        const menuGroupItems = Object.entries(menuGroups[i]);
-        if (i > 0 && menuGroupItems.length)
+        const menuGroupItemMap = new Map(Object.entries(menuGroups[i]));
+        if (removeDisabled)
+            menuGroupItemMap.forEach(removeDisabled);
+        if (i > 0 && menuGroupItemMap.size)
             addSeparator(); // A separator prefaces each non-first, non-empty group
-        for (const [id, title] of menuGroupItems)
+        for (const [id, title] of menuGroupItemMap.entries())
             addItem(id, title);
     }
 }
-
-browser.contextMenus.onClicked.addListener(
-    ({ menuItemId }, tab) => selectTabs(Getter[menuItemId], tab)
-);

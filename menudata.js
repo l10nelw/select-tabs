04d71a9e85c34a4f06535bb1cf4c57cbf49c0c00
Lionel Wong <lionelster@gmail.com>
2022-06-12 20:23:01 +0800

    Update v2.1.2 and tweak menu

--- menudata.js
+++ menudata.js
@@ -5,10 +5,6 @@ export default {
         sameSite__cluster:      'Same Site &Cluster',
         sameSite__descendants:  'Sa&me Site and Descendants',
     },
-    'Directional': {
-        left:                   'To the &Left',
-        right:                  'To the &Right',
-    },
     'Tab tree': {
         descendants:            '&Descendants',
         parent:                 '&Parent',
@@ -16,7 +12,11 @@ export default {
         siblings:               'S&iblings',
         siblings__descendants:  'Si&blings and Descendants',
     },
-    'Time-based': {
+    'Directional': {
+        left:                   'To the &Left',
+        right:                  'To the &Right',
+    },
+    'Temporal': {
         pastHour:               'Accessed in the Past &Hour',
         past24Hours:            'Accessed in the Past 24 H&ours',
         today:                  'Accessed &Today',

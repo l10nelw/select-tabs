96ca85788f464b4e1cb97cbbce222907b5beb644
Lionel Wong <lionelster@gmail.com>
2023-04-04 18:33:12 +0800

    Refactor focus prepping

--- background/select.js
+++ background/select.js
@@ -1,5 +1,3 @@
-import { parent as getParent } from './get.js';
-
 export default async function selectTabs(getter, targetTab) {
     let tabsToSelect = await getter(targetTab);
 
@@ -24,31 +22,21 @@ export default async function selectTabs(getter, targetTab) {
     if (!tabCount)
         return;
 
-    // Focus on target's parent if relevant, otherwise:
-    // focus on either currently focused tab, target, or leftmost tab
-    const parentTab = (isParentGetter && tabCount >= 1) && (await getParent(targetTab))[0];
-    const keepCurrentFocus = !parentTab;
-    prepTabToFocus(tabsToSelect, keepCurrentFocus, parentTab || targetTab);
+    if (tabCount >= 2) {
+        // Set up tabs for browser.tabs.highlight(), which will activate (put focus on) the first tab in array
+        const indexToFocus = (
+            // Move any of these available tabs to the start of the array: parent tab, currently focused tab, target tab
+            // +1 to found indexes so that -1 becomes 0 (falsey)
+            isParentGetter && findIdIndex(tabsToSelect, targetTab.openerTabId) + 1 ||
+            tabsToSelect.findIndex(tab => tab.active) + 1 ||
+            findIdIndex(tabsToSelect, targetTab.id) + 1
+        ) - 1;
+        if (indexToFocus >= 1) // If tab to focus is available and not already first in array
+            [ tabsToSelect[0], tabsToSelect[indexToFocus] ] = [ tabsToSelect[indexToFocus], tabsToSelect[0] ]; // Swap with first tab
+    }
 
     browser.tabs.highlight({ tabs: tabsToSelect.map(tab => tab.index), populate: false });
 }
 
-// Set up `tabs` array for tabs.highlight(), which focuses (activates) the first tab in array.
-// Moves a particular tab, if available, to the start of the `tabs` array.
-// That tab is either: the currently focused tab if `keepCurrentFocus` is true, or the given `tab`.
-// If neither is available, nothing is moved; the first tab in the array is "prepped" for focus.
-function prepTabToFocus(tabs, keepCurrentFocus, tab) {
-    if (tabs.length <= 1)
-        return;
-
-    let indexToFocus = keepCurrentFocus ? tabs.findIndex(t => t.active) : -1;
-
-    if (indexToFocus === -1) {
-        const tabId = tab.id;
-        indexToFocus = tabs.findIndex(t => t.id === tabId);
-    }
-
-    if (indexToFocus >= 1)
-        [ tabs[0], tabs[indexToFocus] ] = [ tabs[indexToFocus], tabs[0] ]; // Swap with first tab
-}
 const findMismatchedIndex = tabs => tabs.findIndex((tab, index) => tab.index !== index);
+const findIdIndex = (tabs, tabId) => tabs.findIndex(tab => tab.id === tabId);

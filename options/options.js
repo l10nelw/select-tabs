cdc2c9f8816a304e08f577f63f146b830cefd176
l10nelw <1300917+l10nelw@users.noreply.github.com>
2024-12-18 16:36:36 +0800

    Rework options

--- options/options.js
+++ options/options.js
@@ -1,48 +1,149 @@
-import { getPreferenceDict, getCommandMap, cleanCommandDescription } from '../common.js';
+import { isOS } from '../common.js';
+import * as Commands from '../commands.js';
 
-(async function populateForm() {
-    const preferenceDict = await getPreferenceDict();
+/** @typedef {import('../common.js').CommandId} CommandId */
+/** @typedef {import('../common.js').CommandInfo} CommandInfo */
+/** @typedef {import('../common.js').CommandDict} CommandDict */
 
-    const $commands = document.getElementById('commands');
-    const $template = $commands.querySelector('template').content;
-    const template = {
-        $heading: $template.firstElementChild,
-        $checkField: $template.querySelector('label'),
-    };
-    $commands.addHeading = title => {
-        const $heading = template.$heading.cloneNode();
-        $heading.textContent = title;
-        $commands.append($heading);
-    };
-    $commands.addField = (name, title, checked) => {
-        const $field = template.$checkField.cloneNode(true);
-        const $input = $field.querySelector('input');
-        $input.name = name;
-        $input.checked = checked;
-        $field.querySelector('span').textContent = cleanCommandDescription(title);
-        $commands.append($field);
-    };
+/** @type {boolean} */ const SUPPORTS_ACCESSKEYS = !isOS('Mac OS');
+/** @type {Element} */ const $form = document.body.querySelector('form');
+/** @type {Element} */ const $tableBody = $form.querySelector('tbody');
+init();
+$form.addEventListener('submit', onFormSubmit);
+$form.addEventListener('input', onFormInput);
+$form.addEventListener('focusin', onFormFocus);
 
-    let currentCategory = '';
-    for (const [id, description] of getCommandMap()) {
-        const [category, title] = description.split(': ');
-        if (category !== currentCategory) {
-            currentCategory = category;
-            $commands.addHeading(category);
-        }
-        $commands.addField(id, title, preferenceDict[id]);
+async function init() {
+    /** @type {DocumentFragment} */ const $template = $form.querySelector('template').content;
+    /** @type {Element} */ const $headingTemplate = $template.firstElementChild;
+    /** @type {Element} */ const $rowTemplate = $template.lastElementChild;
+
+    /** @type {[{ commands: CommandDict, shownTabMenuItems: Set<CommandId> }, Object[]]} */
+    const [{ commands, shownTabMenuItems }, shortcutableCommands] = await Promise.all([ Commands.getData(), browser.commands.getAll() ]);
+
+    for (const { name, shortcut } of shortcutableCommands)
+        commands[name].shortcut = shortcut || '-';
+
+    if (SUPPORTS_ACCESSKEYS) {
+        commands.matchSelectionText.title = 'With Selected Text';
+    } else {
+        $form.querySelector('.accessKey')?.remove(); // In <thead>
+        $headingTemplate.querySelector('.accessKey')?.remove();
+        $rowTemplate.querySelector('.accessKey')?.remove();
+        delete commands.menuRoot;
+        delete commands.matchLinkText;
+        delete commands.matchSelectionText;
     }
-})();
 
-document.body.querySelector('form')
-.addEventListener('submit', async event => {
-    // Save and restart
+    /**
+     * @param {string} id
+     * @returns {string}
+     */
+    const accessKeyFieldLabel = id => `Access key for "${id}" command`;
 
-    const preferenceDict = {};
-    for (const $field of event.target.elements)
-        if ($field.classList.contains('command'))
-            preferenceDict[$field.name] = $field.checked;
+    /**
+     * @param {string} category
+     * @returns {Element}
+     */
+    $tableBody.addHeading = category => {
+        const $heading = $headingTemplate.cloneNode(true);
+        $heading.firstElementChild.textContent = category;
+        $tableBody.append($heading);
+        return $heading;
+    };
 
-    await browser.storage.sync.set(preferenceDict);
+    /**
+     * @param {CommandId} id
+     * @param {string} title
+     * @param {CommandInfo} info
+     * @param {string} info.accessKey
+     * @param {string} info.shortcut
+     * @returns {Element}
+     */
+    $tableBody.addRow = (id, { accessKey, contexts, parentId, shortcut, title }) => {
+        /** @type {boolean} */
+        const isTabMenuItem = parentId && contexts.includes('tab');
+        if (!isTabMenuItem && !SUPPORTS_ACCESSKEYS)
+            return;
+        /** @type {Element} */
+        const $row = $rowTemplate.cloneNode(true);
+
+        // title
+        const $title = $row.querySelector('.title label');
+        $title.textContent = title;
+        $title.htmlFor = id;
+        // showInTabMenu
+        const $showInTabMenu = $row.querySelector('.showInTabMenu input');
+        $showInTabMenu.id = id;
+        $showInTabMenu.name = id;
+        isTabMenuItem ?
+            $showInTabMenu.checked = shownTabMenuItems.has(id) :
+            $showInTabMenu.remove();
+        // accessKey
+        if (SUPPORTS_ACCESSKEYS) {
+            const $accessKey = $row.querySelector('.accessKey input');
+            $accessKey.name = id;
+            $accessKey.value = accessKey;
+            $accessKey.setAttribute('aria-label', accessKeyFieldLabel(title));
+        }
+        // shortcut
+        if (shortcut)
+            $row.querySelector('.shortcut').textContent = shortcut;
+
+        $tableBody.append($row);
+        return $row;
+    };
+
+    // Add rows for commands and categories
+    let currentCategory = '';
+    for (const [id, info] of Object.entries(commands)) {
+        const { category = '' } = info;
+        if (category !== currentCategory) {
+            $tableBody.addHeading(category);
+            currentCategory = category;
+        }
+        $tableBody.addRow(id, info);
+    }
+}
+
+/**
+ * @listens Event#focusin
+ * @param {Event} event
+ */
+async function onFormFocus(event) {
+    const $field = event.target;
+    if ($field.matches('.accessKey input'))
+        $field.select();
+}
+
+/**
+ * @listens Event#input
+ * @param {Event} event
+ */
+async function onFormInput(event) {
+    const $field = event.target;
+    if ($field.matches('.accessKey input'))
+        $field.value = $field.value.toLowerCase();
+}
+
+/**
+ * Save preferences.
+ * @listens Event#submit
+ * @param {Event} event
+ */
+async function onFormSubmit(event) {
+    event.preventDefault();
+
+    const shownTabMenuItems = [];
+    for (const $input of $tableBody.querySelectorAll('.showInTabMenu input'))
+        if ($input.checked)
+            shownTabMenuItems.push($input.name);
+
+    const accessKeys = {};
+    for (const $input of $tableBody.querySelectorAll('.accessKey input'))
+        if ($input.value)
+            accessKeys[$input.name] = $input.value;
+
+    await browser.storage.sync.set({ shownTabMenuItems, accessKeys });
     browser.runtime.reload();
-});
+}

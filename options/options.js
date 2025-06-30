23bc32baa3c5d95bb7b5624129fff77e065eec40
l10nelw <1300917+l10nelw@users.noreply.github.com>
2025-06-30 14:15:04 +0800

    Update options page: improve JSDoc and more
    - Add Shift tip
    - Remove `browser_ui: true` in manifest.json

--- options/options.js
+++ options/options.js
@@ -7,8 +7,9 @@ import * as Commands from '../commands.js';
 /** @typedef {import('../common.js').CommandDict} CommandDict */
 
 /** @type {boolean} */ const SUPPORTS_ACCESSKEYS = !isOS('Mac OS');
-/** @type {Element} */ const $form = document.body.querySelector('form');
-/** @type {Element} */ const $tableBody = $form.querySelector('tbody');
+/** @type {HTMLFormElement} */ const $form = document.body.querySelector('form');
+/** @type {HTMLTableSectionElement} */ const $commandTableBody = $form.querySelector('tbody');
+
 populate();
 $form.addEventListener('submit', onFormSubmit);
 $form.addEventListener('input', onFormInput);
@@ -23,8 +24,8 @@ const relevantProp = type => (type === 'checkbox') ? 'checked' : 'value';
 
 async function populate() {
     const $template = $form.querySelector('template').content;
-    /** @type {Element} */ const $headingTemplate = $template.firstElementChild;
-    /** @type {Element} */ const $rowTemplate = $template.lastElementChild;
+    /** @type {HTMLTableRowElement} */ const $headingTemplate = $template.firstElementChild;
+    /** @type {HTMLTableRowElement} */ const $rowTemplate = $template.lastElementChild;
 
     /** @type {[{ general: object, commands: CommandDict, shownTabMenuItems: Set<CommandId> }, object[]]} */
     const [{ general, commands, shownTabMenuItems }, shortcutableCommands] = await Promise.all([ Commands.getData(), browser.commands.getAll() ]);
@@ -44,61 +45,59 @@ async function populate() {
     }
 
     /**
-     * @param {string} id
-     * @returns {string}
-     */
-    const accessKeyFieldLabel = id => `Access key for "${id}" command`;
-
-    /**
      * @param {string} category
-     * @returns {Element}
+     * @returns {HTMLTableRowElement}
      */
-    $tableBody.addHeading = category => {
+    $commandTableBody.addHeading = category => {
+        /** @type {HTMLTableRowElement} */
         const $heading = $headingTemplate.cloneNode(true);
         $heading.firstElementChild.textContent = category;
-        $tableBody.append($heading);
+        $commandTableBody.append($heading);
         return $heading;
     };
 
     /**
      * @param {CommandId} id
-     * @param {string} title
      * @param {CommandInfo} info
-     * @param {string} info.accessKey
-     * @param {string} info.shortcut
-     * @returns {Element}
+     * @returns {HTMLTableRowElement}
      */
-    $tableBody.addRow = (id, { accessKey, contexts, parentId, shortcut, title }) => {
+    $commandTableBody.addRow = (id, { accessKey, contexts, parentId, shortcut, title }) => {
         /** @type {boolean} */
         const isTabMenuItem = parentId && contexts.includes('tab');
         if (!isTabMenuItem && !SUPPORTS_ACCESSKEYS)
             return;
-        /** @type {Element} */
+        /** @type {HTMLTableRowElement} */
         const $row = $rowTemplate.cloneNode(true);
 
         // title
+        /** @type {HTMLLabelElement} */
         const $title = $row.querySelector('.title label');
         $title.textContent = title;
         $title.htmlFor = id;
+
         // showInTabMenu
+        /** @type {HTMLInputElement} */
         const $showInTabMenu = $row.querySelector('.showInTabMenu input');
         $showInTabMenu.id = id;
         $showInTabMenu.name = id;
-        isTabMenuItem ?
-            $showInTabMenu.checked = shownTabMenuItems.has(id) :
-            $showInTabMenu.remove();
+        isTabMenuItem
+            ? $showInTabMenu.checked = showInTabMenu
+            : $showInTabMenu.remove();
+
         // accessKey
         if (SUPPORTS_ACCESSKEYS) {
+            /** @type {HTMLInputElement} */
             const $accessKey = $row.querySelector('.accessKey input');
             $accessKey.name = id;
             $accessKey.value = accessKey;
-            $accessKey.setAttribute('aria-label', accessKeyFieldLabel(title));
+            $accessKey.setAttribute('aria-label', `Access key for "${title}" command`);
         }
+
         // shortcut
         if (shortcut)
             $row.querySelector('.shortcut').textContent = shortcut;
 
-        $tableBody.append($row);
+        $commandTableBody.append($row);
         return $row;
     };
 
@@ -107,14 +106,15 @@ async function populate() {
     for (const [id, info] of Object.entries(commands)) {
         const { category = '' } = info;
         if (category !== currentCategory) {
-            $tableBody.addHeading(category);
+            $commandTableBody.addHeading(category);
             currentCategory = category;
         }
-        $tableBody.addRow(id, info);
+        $commandTableBody.addRow(id, info);
     }
 
     // General fields
     for (const [key, value] of Object.entries(general)) {
+        /** @type {HTMLInputElement} */
         const $field = $form[key];
         $field[relevantProp($field.type)] = value;
     }
@@ -165,12 +165,13 @@ async function onFormSubmit(event) {
 
     /** @type {string[]} */
     const shownTabMenuItems = [];
-    for (const $input of $tableBody.querySelectorAll('.showInTabMenu input'))
+    for (const $input of $commandTableBody.querySelectorAll('.showInTabMenu input'))
         if ($input.checked)
             shownTabMenuItems.push($input.name);
 
+    /** @type {object.<CommandId, string>} */
     const accessKeys = {};
-    for (const $input of $tableBody.querySelectorAll('.accessKey input'))
+    for (const $input of $commandTableBody.querySelectorAll('.accessKey input'))
         if ($input.value)
             accessKeys[$input.name] = $input.value;
 

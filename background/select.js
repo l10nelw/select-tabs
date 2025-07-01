79d5ac1456cd25f4e529fe1c7a397a7cc9d6cb0b
l10nelw <1300917+l10nelw@users.noreply.github.com>
2025-07-01 18:39:22 +0800

    Implement findClosestToTargetTab

--- background/select.js
+++ background/select.js
@@ -48,17 +48,20 @@ export default async function selectTabs(commandId, targetTab, menuClickInfo) {
     if (!tabCount)
         return;
 
-    // Handle any any switch in active tab status
     // Set up tabs for browser.tabs.highlight() shortly, which will activate the first tab in array
     if (tabCount >= 2) {
-        const indexToActivate = (
-            // Move any of these available tabs to the start of the array
-            // +1 to found indexes so that -1 (not found) becomes 0 (falsey), allowing this OR-operation to work
-            tabsToSelect.findIndex(tab => tab.active) + 1 || // Currently active tab
-            findIdIndex(tabsToSelect, targetTab.id) + 1 // Target tab
+        // Move to the start of the array a suitable tab within tabsToSelect, which meets one of the criteria
+        // +1 to found indexes so that -1 (not found) becomes 0 (falsey), allowing this OR-chain to work
+        const arrayIndexToActivate = (
+            tabsToSelect.findIndex(tab => tab.active) // Active tab in tabsToSelect
+            + 1 ||
+            findIdIndex(tabsToSelect, targetTab.id) // Target tab in tabsToSelect
+            + 1 ||
+            tabsToSelect.indexOf(findClosestToTargetTab(tabsToSelect, targetTab)) // Closest to target tab among tabsToSelect
+            + 1
         ) - 1; // Revert 0 to -1
-        if (indexToActivate > 0) // If tab to activate is available and not already first in array
-            [ tabsToSelect[0], tabsToSelect[indexToActivate] ] = [ tabsToSelect[indexToActivate], tabsToSelect[0] ]; // Swap with first tab
+        if (arrayIndexToActivate > 0) // If tab to activate is not already first in array
+            [ tabsToSelect[0], tabsToSelect[arrayIndexToActivate] ] = [ tabsToSelect[arrayIndexToActivate], tabsToSelect[0] ]; // Swap with first tab
     }
 
     browser.tabs.highlight({ tabs: tabsToSelect.map(tab => tab.index), populate: false });
@@ -76,3 +79,35 @@ const findMismatchedIndex = tabs => tabs.findIndex((tab, arrayIndex) => tab.inde
  * @returns {number}
  */
 const findIdIndex = (tabs, tabId) => tabs.findIndex(tab => tab.id === tabId);
+
+/**
+ * Among candidiate `tabs` (sorted by `tab.index`), find the one positionally closest to the `targetTab`.
+ * Prefer the right in a tie between two tabs.
+ * @param {Tab[]} tabs
+ * @param {Tab} targetTab
+ * @returns {Tab}
+ */
+function findClosestToTargetTab(tabs, targetTab) {
+    const targetTabIndex = targetTab.index;
+    /** @type {Tab} */ let closestTab = tabs.at(-1);
+
+    if (targetTabIndex >= closestTab.index)
+        return closestTab;
+
+    let smallestIndexGap = closestTab.index;
+    for (const tab of tabs) {
+        const tabIndex = tab.index;
+        if (tabIndex === targetTabIndex)
+            return targetTab; // Zero gap
+        const indexGap = Math.abs(tabIndex - targetTabIndex);
+        if (indexGap === smallestIndexGap) // There are two candidiate tabs equally closest to target (target lies midway between them)
+            return tab; // Return the latter tab
+        if (indexGap > smallestIndexGap) // Gap has grown
+            break; // Therefore the closest candidiate tab was already found in the previous iteration
+        // Gap is shrinking, so continue
+        smallestIndexGap = indexGap;
+        closestTab = tab;
+    }
+
+    return closestTab;
+}
